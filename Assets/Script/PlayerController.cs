b8e584f357ea73ad1b9c10e33623e309e4485f9c
Seryjnyy <61786337+Seryjnyy@users.noreply.github.com>
2020-10-03 00:09:47 +0100

    First

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 2.0f;
+    [SerializeField] bool lockCursor = true;
+    [SerializeField] float walkSpeed = 6.0f;
+    [SerializeField] float gravity = -13.0f;
+    [SerializeField][Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
+
+    float cameraPitch = 0.0f;
+    float velocityY = 0.0f;
+    CharacterController controller = null;
+
+    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDirVelocity = Vector2.zero;
+
+
+    void Start() {
+        controller=GetComponent<CharacterController>();
+        if(lockCursor) {
+            Cursor.lockState=CursorLockMode.Locked;
+            Cursor.visible=false;
+        }
+    }
+
+    void Update()
+    {
+        UpdateMouseLook();
+        UpdateMovement();
+    }
+    void UpdateMouseLook() {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y"));
+        
+        transform.Rotate(Vector3.up*mouseDelta.x*mouseSensitivity); // we are rotating the object on its y axis
+        cameraPitch -= mouseDelta.y*mouseSensitivity;
+        cameraPitch=Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+        playerCamera.localEulerAngles=Vector3.right*cameraPitch;
+    }
+
+    void UpdateMovement() {
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize();
+        currentDir=Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        if(controller.isGrounded) 
+            velocityY=0.0f;
+
+        velocityY+=gravity*Time.deltaTime;
+
+
+        Vector3 velocity = (transform.forward*currentDir.y+transform.right*currentDir.x)*walkSpeed + Vector3.up*gravity;
+        controller.Move(velocity*Time.deltaTime);
+    }
+}
---
+++ Assets/doorOpen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpen: MonoBehaviour
+{
+    bool OpenClose;
+    [SerializeField] GameObject latch = null;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O)) {
+            OpenClose=true;
+        }
+        if(OpenClose)
+            RotateDoor();
+    }
+
+    void RotateDoor() {
+        latch.transform.Rotate(new Vector3(0, 90.0f)*Time.deltaTime);
+    }
+}
